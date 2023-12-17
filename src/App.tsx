b0227f4c5cf83a303edaa13d85bbd6c8ae91b160
pompomko <sdw12334@gmail.com>
2023-12-18 08:22:58 +0900

    todo목록 잘나옴

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  type Todos = {
+    id: Number;
+    title: string;
+    contents: string;
+    isDone: Boolean;
+  };
+  const [todos, setTodos] = useState<Todos[]>([
+    {
+      id: 1,
+      title: "제목1",
+      contents: "내용1",
+      isDone: false,
+    },
+    {
+      id: 2,
+      title: "제목2",
+      contents: "내용2",
+      isDone: false,
+    },
+  ]);
   return (
     <div>
-      <h1>to do list만들기</h1>
+      <header style={{ backgroundColor: "#ff99c8" }}>헤더입니다 </header>
+      <main style={{ backgroundColor: "#fcf6bd" }}>
+        <div>
+          제목
+          <input type="text" />
+          내용
+          <input type="text" />
+          <button>입력 </button>
+        </div>
+        <div>
+          <ul>
+            {todos.map((todo) => (
+              <li key={Number(todo.id)}>
+                {todo.title} - {todo.contents}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </main>
+      <footer style={{ backgroundColor: "#d0f4de" }}>푸터입니다</footer>
     </div>
   );
 }
